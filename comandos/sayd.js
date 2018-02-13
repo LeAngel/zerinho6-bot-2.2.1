e6648c63368ac1f27e32f17c10de56e6622276a2
zerinho6 <zerinho6@gmail.com>
2018-02-13 13:46:38 -0200

    Update sayd.js

--- comandos/sayd.js
+++ comandos/sayd.js
@@ -1,12 +1,15 @@
+const Discord = require('discord.js')
 exports.run = (bot, message, args) => {
-		if (message.channel.permissionsFor(bot.user.id).has('MANAGE_MESSAGES')) {
-			return message.channel.send('Eu preciso de permissão pra deletar mensagens para executar esse comando');
-		}
+	let argsJunto = message.content.split(" ").slice(1).join(' ');
+	if (message.guild.member(bot.user).hasPermission('MANAGE_MESSAGES')) {
+	
+	if (argsJunto.length === 0) {
+		message.reply("Você esqueceu de por oq eu devo falar.")
+	}
+	message.delete();
+	message.channel.send(`${argsJunto}`)
+} else {
+	message.channel.send('Eu preciso da permissão "Gerenciar mensagens" pra deletar mensagens para executar esse comando');
 
-		if (args.length === 0) {
-			return message.channel.send('Você esqueceu de botar oq eu devo falar');
-		}
-
-		 message.delete();
-		 message.channel.send(args.join(' '));
+}
 };
