271f84c6fb2063dc304d0f370cdaf851d5d892e5
zerinho6 <zerinho6@gmail.com>
2018-02-13 13:41:57 -0200

    Anti-Crash

--- comandos/lava.js
+++ comandos/lava.js
@@ -1,34 +1,37 @@
-var Jimp = require("jimp")
-const Discord = require("discord.js");
-
-
+const Discord = require("discord.js")
 exports.run = (bot, message, args) => {
+    let argsJunto = message.content.split(" ").slice(1).join(' ')
+    let argsSeparacaoPorBarra = message.content.split(' ').slice(1).join(' ').split(' | ')
+    var Jimp = require("jimp")
 	if(message.content.split(' ').slice(1).join(' ').length < 1) {
 		message.channel.send('Você não escreveu nada...')
 	} else {
 		if(message.content.split(' ').slice(1).join(' ').length > 66) {
 			message.channel.send('Você ultrapassou o limite de 66 caracteres...')
-		} else {
-			if(message.guild.member(bot.user).hasPermission('ATTACH_FILES')) {
+		}
+			if(message.channel.permissionsFor(bot.user.id).has('ATTACH_FILES')) {
+        if(message.channel.permissionsFor(bot.user.id).has('EMBED_LINKS')) {
 				var authorMessage = message
-                    let argsJunto = message.content.split(" ").slice(1).join(' ')
 				message.channel.send(':clock12: | Aguarde...').then(message => {
-			Jimp.read(`http://imgur.com/CWtKE01.png`, function (err, image) {
+			Jimp.read('http://imgur.com/CWtKE01.png', function (err, image) {
 			if (err) message.channel.send('Ocorreu um erro ao criar a imagem...')
-			Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(function (font) {
-			image.print(font, 68, 41, 'O chão é '+ argsJunto, 700)
+			Jimp.loadFont(Jimp.FONT_SANS_16_BLACK).then(function (font) {
+			image.print(font, 10, 60, argsJunto, 300)
 			var aguardeMessage = message
-			image.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
-			message.channel.sendFile(buffer, 'imagem.png', ':frame_photo: | <@' + authorMessage.author.id + '>').then(message => {
-				aguardeMessage.delete()
-			})
-			})
+			let embed = new Discord.RichEmbed().setDescription(':frame_photo: | ' + authorMessage.author).setImage('attachment://imagem.png');
+image.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
+embed.attachFile(new Discord.Attachment(buffer, 'imagem.png'));
+message.channel.send(embed).then(() => {
+    aguardeMessage.delete();
+});
+});
 			})
 			})})
-			} else {
+    }} else {
 				message.channel.send('Eu não tenho a permissão necessária para fazer isso...')
 			}
-		}
+
+
 
 }
 }
