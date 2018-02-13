7b5d8c30d7c6d8722502db81c75398fa06abde59
zerinho6 <zerinho6@gmail.com>
2018-02-13 13:43:15 -0200

    Update lolwallpaper.js

--- comandos/lolwallpaper.js
+++ comandos/lolwallpaper.js
@@ -1,10 +1,11 @@
 const Discord = require("discord.js")
 exports.run = (bot, message, args) => {
+    if(!message.channel.permissionsFor(bot.user.id).has('EMBED_LINKS')) return message.channel.send("Por favor, me dê a permissão EMBED_LINKS para esse e outros comandos funcionarem.");
 let argsJunto = message.content.split(" ").slice(1).join(' ')
 let campeão = argsJunto.toLowerCase()
     if (argsJunto.length !== 0) {
         let embed = new Discord.RichEmbed()
-        .setDescription("**Essas imagens são do site oficial do League Of Legends e podem estar desatualizadas, escreva o nome do campeão totalmente igual e com letras minusculas, caso o nome tenha espaço escreva tudo junto, exemplo: ze.lolwallpaper jarvaniv**")
+        .setDescription("**Essas imagens são do site oficial do League Of Legends e podem estar desatualizadas, escreva o nome do campeão totalmente igual, caso o nome tenha espaço escreva tudo junto, exemplo: ze.lolwallpaper jarvaniv**")
      .setImage(`https://lolstatic-a.akamaihd.net/game-info/1.1.9/images/champion/backdrop/bg-${campeão}.jpg`)
                .setTimestamp()
                .setColor(message.member.highestRole.color)
