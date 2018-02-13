069b14bceb3aae0a7132cba083d3c943102c6c8a
zerinho6 <zerinho6@gmail.com>
2018-02-13 13:39:34 -0200

    ah foda-se vcs ja entenderam

--- comandos/github.js
+++ comandos/github.js
@@ -1,3 +1,9 @@
+const Discord = require("discord.js");
 exports.run = (bot, message) => {
-	    message.reply('https://github.com/zerinho6/Zerinho6-Bot/tree/master');
+    if(!message.channel.permissionsFor(bot.user.id).has('EMBED_LINKS')) return message.channel.send("Por favor, me dê a permissão EMBED_LINKS para esse e outros comandos funcionarem.");
+  let embed = new Discord.RichEmbed()
+  .setDescription("https://github.com/zerinho6/zerinho6-bot-2.0")
+  .setTimestamp()
+	.setAuthor(message.author.username, message.author.avatarURL)
+  message.channel.send({embed})
 };
