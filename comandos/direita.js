3c035ce73a9e006b80b668f2f061e63e3ac08467
zerinho6 <zerinho6@gmail.com>
2018-02-13 13:33:20 -0200

    Delete direita.js

--- comandos/direita.js
+++
@@ -1,9 +0,0 @@
-const Discord = requrie('discord.js');
-exports.run = (bot, msg) => {
-	 let embed = new Discord.RichEmbed()
-    .setAuthor(message.author.username, message.author.avatarURL)
-    .setTimestamp()
-    .setTitle("direita")
-    .setImage("https://imgur.com/nzyzZHq.png")
-    message.channel.send({embed});         
-};
