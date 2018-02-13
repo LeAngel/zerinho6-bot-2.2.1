bbe450ce592991b07524de69de664a2b6f4be3c9
zerinho6 <zerinho6@gmail.com>
2018-02-13 13:44:47 -0200

    Update oauth.js

--- comandos/oauth.js
+++ comandos/oauth.js
@@ -1,19 +1,19 @@
-const Discord = require("discord.js");
-var moment = require('moment');
-moment.locale('pt-BR');
+const Discord = require("discord.js")
 exports.run = (bot, message, args) => {
+    if(!message.channel.permissionsFor(bot.user.id).has('EMBED_LINKS')) return message.channel.send("Por favor, me dê a permissão EMBED_LINKS para esse e outros comandos funcionarem.");
     let parts = message.content.split(' ');
     let argsJunto = message.content.split(" ").slice(1).join(' ')
-    let usermentioned = message.mentions.users.first();
-        if (usermentioned === undefined) {
-            message.channel.send('O usuário mencionado não é um bot. (ou ninguém foi mencionado)').then(message => {
-                                setTimeout(() => {message.delete()}, 5000)
-                            })
-        } else if (usermentioned.bot) {
-            message.channel.send(' O link de convite para o bot **' + usermentioned.username + '** é: **<https://discordapp.com/oauth2/authorize?&client_id=' + usermentioned.id + '&scope=bot>**.');
+    if (argsJunto.length !== 0) {
+        let embed = new Discord.RichEmbed()
+        .setColor(message.member.highestRole.color)
+        .setAuthor("Solicitado por " + message.author.username, message.author.avatarURL)
+          .setTimestamp()
+          .setThumbnail(`http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${argsJunto}.png`)
+            .setDescription(`Aperte no nome do campeão para abrir o link\n[**\n${argsJunto}**](http://gameinfo.br.leagueoflegends.com/pt/game-info/champions/${argsJunto}/)`);
+            message.channel.send({embed});
         } else {
-            message.channel.send('O usuário mencionado não é um bot. (ou ninguém foi mencionado)').then(message => {
-                                setTimeout(() => {message.delete()}, 5000)
+            message.channel.send('A sintaxe do comando está incorreta. Sintaxe correta: `' + parts[0] + ' Ahri`').then(message => {
+                            	setTimeout(() => {message.delete()}, 5000)
                             })
         }
 }
