e441e7393cefd983949ef4a906bca322156bdb2c
zerinho6 <zerinho6@gmail.com>
2018-02-13 13:39:05 -0200

    Atualizando para a versão 2.0.1

--- comandos/gif.js
+++ comandos/gif.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js")
 exports.run = (bot, message, args) => {
+  if(!message.channel.permissionsFor(bot.user.id).has('EMBED_LINKS')) return message.channel.send("Por favor, me dê a permissão EMBED_LINKS para esse e outros comandos funcionarem.");
 var giphy = require( 'giphy' )('a4cf068bc86a4a939ecb883414318a2c');
 giphy.search({
     q: message.content.split(" ").slice(1).join("+"),
@@ -11,7 +12,11 @@ giphy.search({
     } else if (res.data.length < 0) {
      message.reply("Você não botou a pesquisa")
     } else if(res.data.length > 0) {
-        message.reply(res.data[0].images.original.url)
+      		let embed = new Discord.RichEmbed()
+           .setImage(res.data[0].images.original.url)
+           .setTimestamp()
+          	.setAuthor(message.author.username, message.author.avatarURL)
+          message.channel.send({embed})
     }
 });
         }
