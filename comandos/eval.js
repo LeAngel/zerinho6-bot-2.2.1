27157479a60b4bfbb5eeb81b1455a725f54d7c65
zerinho6 <zerinho6@gmail.com>
2018-02-15 10:55:47 -0200

    Update eval.js

--- comandos/eval.js
+++ comandos/eval.js
@@ -8,7 +8,7 @@ const c = ["#faff3b","#faff12","#faff00","#e4ff00","#e6fa28","#ebfa4c","#f3ff7b"
 const f = ['Você é burro.','Aprende a Programar.','Pqp velho.','Sai daqui!','Q horror.','Lê a porra da Docs.','que burro da 0 pra ele,kkj','Não','Irineu, quem errou o codigo não fui eu']
 exports.run = (bot, message, args) => {
   const code = message.content.split(' ').slice(1).join(' ')
-    if(message.author.id !== "134292889177030657") return;
+    if(message.author.id !== "seu_id_aqui") return;
 if (message.content.includes('config.token')) {
             message.channel.send('Raio protetor... Protegi meu token de ser exibido!');
         } else {
